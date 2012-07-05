8b4bc7442fd54c32e06aecf6d2e1dc16f14ea2e6
maitaivw <maitaivw@2387b160-4621-4e8a-be36-40ea10d72da2>
2012-07-05 15:52:59 +0000

    add missing sources from previous ci
    git-svn-id: svn://svn.code.sf.net/p/qtvlm/code/trunk@1242 2387b160-4621-4e8a-be36-40ea10d72da2

---
+++ src/Dialogs/routeInfo.cpp
@@ -0,0 +1,47 @@
+#include "routeInfo.h"
+#include "ui_routeInfo.h"
+#include "Util.h"
+
+
+routeInfo::routeInfo(myCentralWidget *parent, ROUTE *route) :
+    QDialog(parent)
+{
+    this->setWindowFlags(Qt::Tool);
+    setupUi(this);
+    Util::setFontDialog(this);
+    this->route=route;
+    this->setWindowTitle(QObject::tr("Information au point d'interpolation pour ")+route->getName());
+    //this->activateWindow();
+}
+void routeInfo::setValues(double twd, double tws, double twa, double bs, double hdg, double cnm, double dnm)
+{
+    TWD->setValue(twd);
+    TWS->setValue(tws);
+    TWA->setValue(qAbs(twa));
+    BS->setValue(bs);
+    HDG->setValue(hdg);
+    CNM->setValue(cnm);
+    DNM->setValue(dnm);
+    double Y=90-qAbs(twa);
+    double a=tws*cos(degToRad(Y));
+    double b=tws*sin(degToRad(Y));
+    double bb=b+bs;
+    double aws=sqrt(a*a+bb*bb);
+    double awa=90-radToDeg(atan(bb/a));
+    AWA->setValue(awa);
+    AWS->setValue(aws);
+    if(twa>=0)
+        amure->setText(tr("Babord amure"));
+    else
+        amure->setText(tr("Tribord amure"));
+}
+
+routeInfo::~routeInfo()
+{
+}
+void routeInfo::closeEvent(QCloseEvent *)
+{
+    QPoint position=this->pos();
+    route->setShowInterpolData(false);
+    this->move(position);
+}
---
+++ src/Dialogs/routeInfo.h
@@ -0,0 +1,23 @@
+#ifndef ROUTEINFO_H
+#define ROUTEINFO_H
+
+#include <QDialog>
+#include "ui_routeInfo.h"
+#include "mycentralwidget.h"
+#include "route.h"
+
+class routeInfo : public QDialog, public Ui::routeInfo
+{
+    Q_OBJECT
+    
+public:
+    routeInfo(myCentralWidget *parent, ROUTE *route);
+    ~routeInfo();
+    void setValues(double twd, double tws, double twa, double bs, double hdg, double cnm, double dnm);
+protected:
+    void closeEvent(QCloseEvent *event);
+private:
+    ROUTE *route;
+};
+
+#endif // ROUTEINFO_H
