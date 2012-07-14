424906c7440d2f1b6ec2e963348809965e52f86e
maitaivw <maitaivw@2387b160-4621-4e8a-be36-40ea10d72da2>
2012-07-14 15:33:30 +0000

    up version (3.2.8-patch2)
    git-svn-id: svn://svn.code.sf.net/p/qtvlm/code/trunk@1256 2387b160-4621-4e8a-be36-40ea10d72da2

--- src/Version.h
+++ src/Version.h
@@ -25,7 +25,7 @@ Copyright (C) 2008 - Jacques Zaninetti - http://zygrib.free.fr
 #define VERSION_H
 
 #define QTVLM_VERSION_NUM   "3.2"
-#define QTVLM_SUB_VERSION_NUM   "8 patch1+"
+#define QTVLM_SUB_VERSION_NUM   "8 patch2"
 #define QTVLM_VERSION_DATE  __DATE__
 
 #ifdef __WIN_QTVLM
