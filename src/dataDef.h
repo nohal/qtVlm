2bcbbe6e2f20389b67a56d2acc075435f57630ea
oxygen77 <oxygen77@2387b160-4621-4e8a-be36-40ea10d72da2>
2013-01-06 16:36:42 +0000

    undef QTVLM_WITH_TEST
    git-svn-id: svn://svn.code.sf.net/p/qtvlm/code/branches/qtVlm_dev@1565 2387b160-4621-4e8a-be36-40ea10d72da2

--- src/dataDef.h
+++ src/dataDef.h
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QColor>
 #include <QMap>
 
-#define __QTVLM_WITH_TEST
+//#define __QTVLM_WITH_TEST
 
 extern QMap<QString,QString> appFolder;
 
