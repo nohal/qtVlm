2919b284c8090e6a9dd6d3a562461bfe26a442d7
maitaivw <maitaivw@2387b160-4621-4e8a-be36-40ea10d72da2>
2013-08-22 06:12:25 +0000

    Update qt translation
    git-svn-id: svn://svn.code.sf.net/p/qtvlm/code/branches/qtVlm_dev@1793 2387b160-4621-4e8a-be36-40ea10d72da2

--- src/LoadGribFile.cpp
+++ src/LoadGribFile.cpp
@@ -195,7 +195,7 @@ void LoadGribFile::requestFinished ( QNetworkReply* inetReply)
         vers.append(".");
         vers.append(QTVLM_SUB_VERSION_NUM);
         vers.remove("+");
-        if(vers.contains("beta") && strbuf=="3.3.5") return;
+        if(vers.contains("beta") && strbuf=="3.4.0") return;
         if(vers.left(strbuf.size())!=strbuf)
         {
             QString m=tr("Vous n'utilisez pas la derniere version de qtVlm: ")+strbuf;
