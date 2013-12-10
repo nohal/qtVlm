a3faa6850f99e622f7c7883fb34eeae801760aac
maitaivw <maitaivw@2387b160-4621-4e8a-be36-40ea10d72da2>
2013-12-10 11:48:23 +0000

    remove a useless variable in boat.h
    git-svn-id: svn://svn.code.sf.net/p/qtvlm/code/branches/qtVlm_dev@2085 2387b160-4621-4e8a-be36-40ea10d72da2

--- src/boat.h
+++ src/boat.h
@@ -131,7 +131,6 @@ class boat: public BoatInterface
         void setSetKeys(QList<QString> keys) { barrierKeys = keys; }
         void cleanBarrierList(void);
         bool cross(QLineF line);
-        void setPoiCircle(double lon, double lat);
 
 public slots:
         void slot_projectionUpdated();
@@ -262,7 +261,6 @@ public slots:
         double declinaison;
         double minSpeedForEngine;
         double speedWithEngine;
-        QPointF poiCircle;
 };
 Q_DECLARE_TYPEINFO(boat,Q_MOVABLE_TYPE);
 #endif // BOAT_H
