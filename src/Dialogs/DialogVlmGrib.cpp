71a6ed7c2d542f0b08b7bee797a841a7dfbff12c
maitaivw <maitaivw@2387b160-4621-4e8a-be36-40ea10d72da2>
2014-01-08 08:12:06 +0000

    Revert one of the modification for mac/os
    git-svn-id: svn://svn.code.sf.net/p/qtvlm/code/branches/qtVlm_dev@2173 2387b160-4621-4e8a-be36-40ea10d72da2

--- src/Dialogs/DialogVlmGrib.cpp
+++ src/Dialogs/DialogVlmGrib.cpp
@@ -148,14 +148,13 @@ bool DialogVlmGrib::gribFileReceived(QByteArray * content)
     filename=gribPath+"/"+filename;
     if(Settings::getSetting("askGribFolder",1)==1)
     {
-//#ifndef __MAC_QTVLM
-//        filename = QFileDialog::getSaveFileName(this,
-//                         tr("Sauvegarde du fichier GRIB"), filename, "Grib (*.grb)");
-//#else
-//        filename = QFileDialog::getSaveFileName(this,
-//                         tr("Sauvegarde du fichier GRIB"), filename, "Grib (*.grb)",0,QFileDialog::DontUseNativeDialog);
-//#endif
-        filename = saveDialog->getSaveFileName(0,tr("Sauvegarde du fichier GRIB"), filename, "Grib (*.grb)",0,QFileDialog::DontUseSheet);
+#ifndef __MAC_QTVLM
+        filename = QFileDialog::getSaveFileName(this,
+                         tr("Sauvegarde du fichier GRIB"), filename, "Grib (*.grb)");
+#else
+        filename = QFileDialog::getSaveFileName(this,
+                         tr("Sauvegarde du fichier GRIB"), filename, "Grib (*.grb)",0,QFileDialog::DontUseNativeDialog);
+#endif
 
     }
 
