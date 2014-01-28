d152ebbcc6159ac63f8eb7397c66e73d18dacd2c
maitaivw <maitaivw@2387b160-4621-4e8a-be36-40ea10d72da2>
2014-01-28 17:07:07 +0000

    Version 3.4.4 beta3
    git-svn-id: svn://svn.code.sf.net/p/qtvlm/code/branches/qtVlm_dev@2218 2387b160-4621-4e8a-be36-40ea10d72da2

--- src/plugins/newVlmBoard/BoardVlmNew.cpp
+++ src/plugins/newVlmBoard/BoardVlmNew.cpp
@@ -66,7 +66,7 @@ void BoardVlmNew::initBoard(MainWindowInterface *main)
 #ifdef __MAC_QTVLM
     if(main->getSettingApp(fusionStyle).toInt()!=1) /*only if not in fusion mode*/
     {
-        int offsetX=0;
+        int offsetX=1;
         int offsetY=2;
         dial->move(dial->x()+offsetX,dial->y()+offsetY);
     }
